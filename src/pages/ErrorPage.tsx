e2f34d256afac817573e0cba4644a565335b798f
Bipin Kumar <8kr.bipin@gmail.com>
2023-12-26 01:07:08 +0530

    modified error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
-
-interface IRouteError extends Error {
-  statusText?: string;
-}
 
 export default function ErrorPage() {
-  const error = useRouteError() as IRouteError;
-
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
