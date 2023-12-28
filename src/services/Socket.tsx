827d5ff79c62fbbfb9b6f55a831a3ccfcea9330d
Devashish Taneja <dtaneja@enphaseenergy.com>
2023-12-28 20:08:48 +0530

    Fix url for production

--- src/services/Socket.tsx
+++ src/services/Socket.tsx
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:8080/', { path: '/api/socket.io', transports: ['websocket'] });
-
+export const socket = io(process.env.NODE_ENV === 'production' ? 'https://cardarena.iugaming.com' : 'http://localhost:8080/', { path: '/api/socket.io', transports: ['websocket'] });
 const JoinGame = (playerName?: string, gameId?: string) => {
     if (!gameId) {
         gameId = generateHexId(8);
